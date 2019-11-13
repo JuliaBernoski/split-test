99090d23e36bd0bd5f05bf96e57636737228232a
Julia Bernoski <juliabernoski@Julias-Macbook-Pro.local>
2019-11-13 15:04:25 -0500

    name changes

--- MultiplePage/index.js
+++ MultiplePage/index.js
@@ -13,7 +13,7 @@ var json = {
             questions: [
                 {
                     //Question 1
-                    name: "name",
+                    name: "Question1",
                     type: "text",
                     title: "What is the biggest challenge you’ve faced in your current or previous role? How did you overcome this challenge? What was the outcome?",                   
                     placeHolder: "",
@@ -21,7 +21,7 @@ var json = {
                 },
                 {
                     //Question 2
-                    name: "name",
+                    name: "Question2",
                     type: "text",
                     title: "What are your goals for professional development? How will getting this job help you advance toward your professional development goals?",                   
                     placeHolder: "",
@@ -29,7 +29,7 @@ var json = {
                 },
                 {
                     //Question 3
-                    type: "checkbox",
+                    type: "Question3",
                     name: "Fun Job",
                     title: "What makes a job fun and motivating for you?",
                     isRequired: true,
@@ -46,7 +46,7 @@ var json = {
                 },
                 {
                     //Question 4
-                    name: "name",
+                    name: "Question4",
                     type: "text",
                     title: "What career challenges have prepared you for the challenges you would face in this job?",                   
                     placeHolder: "",
@@ -54,7 +54,7 @@ var json = {
                 },
                 {
                     //Question 5
-                    name: "name",
+                    name: "Question5",
                     type: "text",
                     title: "Have you ever had to manage multiple deadlines set for the same day or week? How did you do it?",                   
                     placeHolder: "",
@@ -67,7 +67,7 @@ var json = {
             questions: [
                 {
                     //Question 6
-                    name: "name",
+                    name: "Question6",
                     type: "text",
                     title: "What was the most frustrating part of your current role? Have you ever taken steps to try and make this part of your role less frustrating?",                   
                     placeHolder: "",
@@ -75,7 +75,7 @@ var json = {
                 },
                 {
                     //Question 7
-                    name: "name",
+                    name: "Question7",
                     type: "text",
                     title: "What does your typical work week look like in terms of responsibilities? How have these responsibilities changed over time?",                   
                     placeHolder: "",
@@ -83,7 +83,7 @@ var json = {
                 },
                 {
                     //Question 8
-                    name: "name",
+                    name: "Question8",
                     type: "text",
                     title: "What salary range do you expect from your next role?",                   
                     placeHolder: "",
@@ -91,7 +91,7 @@ var json = {
                 },
                 {
                     //Question 9
-                    name: "name",
+                    name: "Question9",
                     type: "text",
                     title: "When can you start if you’re hired?",                   
                     placeHolder: "",
@@ -99,7 +99,7 @@ var json = {
                 },
                 {
                     //Question 10
-                    type: "radiogroup",
+                    type: "Question10",
                     name: "interview",
                     title: "Are you currently interviewing with any other companies?",
                     isRequired: true,
