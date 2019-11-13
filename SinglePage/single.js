b7edc1179fbb254e1305231f9a5413378d8104b9
JuliaBernoski <55218695+JuliaBernoski@users.noreply.github.com>
2019-11-13 15:18:57 -0500

    Update single.js
    changing to single page

--- SinglePage/single.js
+++ SinglePage/single.js
@@ -1,43 +1,111 @@
+//Sruvey For Web Development Role
 
 Survey
     .StylesManager
     .applyTheme("default");
 
 var json = {
-    title: "American History",
-    pages: [{
-        questions: [
-            {
-                type: "radiogroup",
-                name: "civilwar",
-                title: "When was the Civil War?",
-                choices: [
-                    "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                ],
-                correctAnswer: "1850-1900"
-            },
-            {
-                type: "radiogroup",
-                name: "libertyordeath",
-                title: "Who said 'Give me liberty or give me death?'",
-                choicesOrder: "random",
-                choices: [
-                    "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                ],
-                correctAnswer: "Patrick Henry"
-            },
-            {
-                type: "radiogroup",
-                name: "magnacarta",
-                title: "What is the Magna Carta?",
-                choicesOrder: "random",
-                choices: [
-                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                ],
-                correctAnswer: "The foundation of the British parliamentary system"
-            }
-        ]
-    }],
+    title: "Employment Screening",
+    pages: [
+        {
+            //Page 1
+            questions: [
+                {
+                    //Question 1
+                    name: "Question1",
+                    type: "text",
+                    title: "What is the biggest challenge you’ve faced in your current or previous role? How did you overcome this challenge? What was the outcome?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 2
+                    name: "Question2",
+                    type: "text",
+                    title: "What are your goals for professional development? How will getting this job help you advance toward your professional development goals?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 3
+                    type: "Question3",
+                    name: "Fun Job",
+                    title: "What makes a job fun and motivating for you?",
+                    isRequired: true,
+                    hasNone: true,
+                    colCount: 4,
+                    choices: [
+                        "The People",
+                        "Interesting Projects",
+                        "Completing a Project",
+                        "Gaining New Clients",
+                        "Being Able to work Independently",
+                        "Working on my own projects"
+                    ]
+                },
+                {
+                    //Question 4
+                    name: "Question4",
+                    type: "text",
+                    title: "What career challenges have prepared you for the challenges you would face in this job?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 5
+                    name: "Question5",
+                    type: "text",
+                    title: "Have you ever had to manage multiple deadlines set for the same day or week? How did you do it?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 6
+                    name: "Question6",
+                    type: "text",
+                    title: "What was the most frustrating part of your current role? Have you ever taken steps to try and make this part of your role less frustrating?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 7
+                    name: "Question7",
+                    type: "text",
+                    title: "What does your typical work week look like in terms of responsibilities? How have these responsibilities changed over time?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 8
+                    name: "Question8",
+                    type: "text",
+                    title: "What salary range do you expect from your next role?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 9
+                    name: "Question9",
+                    type: "text",
+                    title: "When can you start if you’re hired?",                   
+                    placeHolder: "",
+                    isRequired: true
+                },
+                {
+                    //Question 10
+                    type: "Question10",
+                    name: "interview",
+                    title: "Are you currently interviewing with any other companies?",
+                    isRequired: true,
+                    colCount: 1,
+                    choices: [
+                         "Yes",
+                         "No"
+                    ]
+                }
+            ]
+        }
+    ],
     completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
 
@@ -46,7 +114,8 @@ window.survey = new Survey.Model(json);
 survey
     .onComplete
     .add(function (result) {
-        document.location = "singlethanks.html";
+        document
+            .location = "singlethanks.html";
     });
 
 $("#surveyElement").Survey({ model: survey });
